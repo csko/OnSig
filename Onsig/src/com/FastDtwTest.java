afc23fdcee4595e1ec9b8d27bed8e4d8b9777481
Bordé Sándor <sanyi87@gmail.com>
2011-11-07 08:00:44 +0100

    évközi prezentáció, osztályozó futása az összes aláírásra

--- Onsig/src/com/FastDtwTest.java
+++ Onsig/src/com/FastDtwTest.java
@@ -40,15 +40,40 @@ public class FastDtwTest
     */
       public static void main(String[] args)
       {
-          TimeSeries []tg = new TimeSeries[16];
-          for ( int i = 0 ; i < 16  ; i++ ) {
-              final TimeSeries tsI = new TimeSeries("../data-deriv/genuine/001_"+(i+1)+".HWR", false, false, ' ');
+
+          boolean DEBUG = false;
+          
+          double FRR = 0.0;
+          double testSize = 0;
+          double determinedGenuine = 0;
+
+          double maxgood = 0;
+          double mingood = 0;
+          double avggood = 0;
+
+          for ( int j = 0 ; j < 16 ; j++ ) {
+          try {
+          TimeSeries []tg = new TimeSeries[12];
+          //String filenamepref = "0";
+          //filenamepref += (j<10)?"0"+(j+1):(j+1);
+          //filenamepref += "_";
+          for ( int i = 0 ; i < 12  ; i++ ) {
+              //String filename = filenamepref;
+              /*if ( i < 10 ) {
+                  filename += "0"; 
+                  System.out.println(i);
+              }*/
+
+              /*filename += (i+1);
+              System.out.println(filename);*/
+              final TimeSeries tsI = new TimeSeries("../data-deriv/genuine/00"+(j+1)+"_"+(i+1)+".HWR", false, false, ' ');
               tg[i] = tsI;
           }
 
           TrainingSignature tsGenuine = new TrainingSignature(tg, Integer.parseInt(args[0]));
 
-          System.out.println(tsGenuine);
+          if ( DEBUG )
+            System.out.println(tsGenuine);
 
 
           
@@ -57,7 +82,9 @@ public class FastDtwTest
 
           ArrayList<String> currentForgeries = new ArrayList<String>();
           for ( int i = 0 ; i < files.length ; i++ ) {
-              if ( Pattern.matches("[0-9]*_001_[0-9]*.HWR", files[i]) )
+              /*String filename = filenamepref;
+              filename += (i<10)?"0"+(i+1):(i+1);*/
+              if ( Pattern.matches("[0-9]*_00"+(i+1)+"_[0-9]*.HWR", files[i]) )
                 currentForgeries.add(files[i]);
               
           }
@@ -69,24 +96,21 @@ public class FastDtwTest
           }
           TrainingSignature tsForgery = new TrainingSignature(tf, Integer.parseInt(args[0]));
 
+          if ( DEBUG ) {
           System.out.println(tsForgery);
 
           System.out.println("Min: Genuine(" + tsGenuine.getMinDistance() +") >? Forgery(" + tsForgery.getMinDistance() + ") " + (tsGenuine.getMinDistance()>tsForgery.getMinDistance()));
           System.out.println("Max: Genuine(" + tsGenuine.getMaxDistance() +") >? Forgery(" + tsForgery.getMaxDistance() + ") " + (tsGenuine.getMaxDistance()>tsForgery.getMaxDistance()));
           System.out.println("Avg: Genuine(" + tsGenuine.getAverageDistance() +") >? Forgery(" + tsForgery.getAverageDistance() + ") " + (tsGenuine.getAverageDistance()>tsForgery.getAverageDistance()));
+          }
 
-          double FRR = 0.0;
-          double testSize = 0;
-          double determinedGenuine = 0;
-
-          double maxgood = 0;
-          double mingood = 0;
-          double avggood = 0;
 
           System.out.println();
-          for ( int i = 16 ; i < 24 ; i++ ) {
+          for ( int i = 12 ; i < 24 ; i++ ) {
+            /*String filename = filenamepref;
+            filename += (i<10)?"0"+(i+1):(i+1);*/
             testSize++;
-            final TimeSeries tsI = new TimeSeries("../data-deriv/genuine/001_"+(i+1)+".HWR", false, false, ' ');
+            final TimeSeries tsI = new TimeSeries("../data-deriv/genuine/00"+(j+1)+"_"+(i+1)+".HWR", false, false, ' ');
             DTWClassifier cl = new DTWClassifier(tsGenuine, tsForgery);
             int min = cl.isValidSignature(tsI, Integer.parseInt(args[0]), DTWClassifier.MIN);
             int max = cl.isValidSignature(tsI, Integer.parseInt(args[0]), DTWClassifier.MAX);
@@ -94,12 +118,20 @@ public class FastDtwTest
             mingood += min;
             maxgood += max;
             avggood += avg;
-            System.out.println("A 001_"+(i+1)+".HWR aláírás:");
-            System.out.println("A legkisebb távokat összevetve: " + min);
-            System.out.println("A legnagyobb távokat összevetve: " + max);
-            System.out.println("A átlagos távokat összevetve: " + avg);
             if ( min + max + avg >= 2 )
                 determinedGenuine++;
+
+            if ( DEBUG ) {
+                //System.out.println("A "+filename+".HWR aláírás:");
+                System.out.println("A legkisebb távokat összevetve: " + min);
+                System.out.println("A legnagyobb távokat összevetve: " + max);
+                System.out.println("A átlagos távokat összevetve: " + avg);
+            }
+          }
+          } catch (java.lang.InternalError e) {
+              System.out.println("Nincs ilyen file.");
+              continue;
+          }
           }
 
           System.out.println("Helyes aláírás jól osztályozva: " + (determinedGenuine/testSize));
